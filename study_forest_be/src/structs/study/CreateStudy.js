c498d085acc5ee526263b6c01d8214440af0ac0a
root <root@DESKTOP-UE7169K>
2024-08-16 11:23:01 +0900

    feat: 습관 이름 수정 API 외 3개 추가 습관 이름 수정, 습관 삭제, 습관 리스트 조회, 완료한 습관 추가 API 추가 참고: 습관 삭제와 완료한 습관 추가 API에서 body가 없어도 정상 작동함

--- study_forest_be/src/structs/study/CreateStudy.js
+++ study_forest_be/src/structs/study/CreateStudy.js
@@ -1,9 +1,11 @@
-import *as s from 'superstruct'
+import * as s from "superstruct";
 
-export const CreateStudy = s.object({
-    nickName: s.size(s.string(), 2, 12),
-    studyName: s.size(s.string(), 2, 20),
-    description: s.size(s.string(), 0, 100),
-    background: s.size(s.string(), 0, 50),
-    password: s.size(s.string(), 8, 24),
-  });
+export const CreateStudyBody = {
+  nickName: s.size(s.string(), 2, 12),
+  studyName: s.size(s.string(), 2, 20),
+  description: s.size(s.string(), 0, 100),
+  background: s.size(s.string(), 0, 50),
+  password: s.size(s.string(), 8, 24),
+}
+
+export const CreateStudy = s.object({...CreateStudyBody});
