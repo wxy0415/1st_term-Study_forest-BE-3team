77381af99b5e6b3fc74fff2dec6adfbd332ccaf4
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-08-22 09:24:04 +0900

    test : 오늘의 습관 정보 확인

--- src/utils/timeRangeHandler.js
+++ src/utils/timeRangeHandler.js
@@ -5,12 +5,12 @@ const day = String(today.getDate()).padStart(2, "0");
 
 const formattedDate = `${year}-${month}-${day}`;
 
-const todayUCT =  new Date(
-  new Date(formattedDate).getTime() - 9 * 60 * 60 * 1000
+const todayUCT = new Date(
+  new Date(formattedDate).getTime() - 12 * 60 * 60 * 1000
 );
 
 const nextDayUCT = new Date(
   new Date(formattedDate).getTime() + 15 * 60 * 60 * 1000
 );
 
-export { todayUCT, nextDayUCT }
+export { todayUCT, nextDayUCT };
